0b353e371be80e881d0f284b5c6380957d5272bd
simonlynen <simon.lynen@mavt.ethz.ch>
2013-05-10 16:53:57 +0200

    minor

--- runtest.sh
+++ runtest.sh
@@ -4,4 +4,4 @@ set -e
 
 PACKAGE_DIR=$(rospack find slam_plus_plus)
 
-$SLAM_PP_URL/slam_plus_plus_src/bin/slam_plus_plus -i data/manhattanOlson3500.txt --pose-only
+$PACKAGE_DIR/slam_plus_plus_src/bin/slam_plus_plus -i data/manhattanOlson3500.txt --pose-only
